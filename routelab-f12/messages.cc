3f2ecaa21e4c6f0ee0a21f36ec3fb222ee8536f6
qrtrmstr12 <QM22may()josh.miner12@gmail.com>
2014-12-02 15:40:25 -0500

    Add some linkstate stuff to the messages

--- routelab-f12/messages.cc
+++ routelab-f12/messages.cc
@@ -13,10 +13,55 @@ RoutingMessage & RoutingMessage::operator=(const RoutingMessage &rhs)
     return *this;
 }
 
-//#if defined(GENERIC) // TODO
+#if defined(GENERIC) // TODO
 ostream &RoutingMessage::Print(ostream &os) const
 {
   os << "RoutingMessage()";
   return os;
 }
-//#endif
+#endif
+
+#if defined(LINKSTATE)
+
+RoutingMessage::RoutingMessage()	// Empty constructor
+{
+}
+
+RoutingMessage::RoutingMessage(int s, int d, int l)	// Initialize with src, dest, and latency
+{
+	src = s;
+	dest = d;
+	latency = l;
+}
+
+RoutingMessage::RoutingMessage(const RoutingMessage &rhs)	// Copy from one to another
+{
+	src = rhs.src;
+	dest = rhs.dest;
+	latency = rhs.latency;
+}
+
+ostream &RoutingMessage::Print(ostream &os) const	// Print the information
+{ 
+	os << "path from " << src << " to " << dest << " of " << latency << endl; 
+	return os; 
+}
+
+int 
+RoutingMessage::GetSrc() const
+{ 
+	return src; 	// Return the source of this message
+}
+
+int
+RoutingMessage::GetDest() const	
+{ 
+	return dest; // Return the destination of this message
+}
+
+int 
+RoutingMessage::GetLatency() const 
+{ 
+	return latency; // Return the latency of this link
+}
+#endif
